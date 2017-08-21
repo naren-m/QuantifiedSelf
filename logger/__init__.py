9f223c478ea044742315cdc9f212947cd73c25fe
Naren Mudivarthy <nmudivar@cisco.com>
2017-08-21 10:06:11 -0700

    Confurable logging to screen and print

--- logger/__init__.py
+++ logger/__init__.py
@@ -1,34 +1,44 @@
 import logging
 import logging.handlers
+import sys
+import config as cfg
 
 LOG_FILENAME = 'logs/application.log'
 FORMAT = '%(asctime)s-%(levelname).4s-%(funcName)s()-%(filename)s:%(lineno).3d : %(message)s'
 
-app_logger = logging.getLogger(__name__)
-# file handler to keep size of log file in check
-# handler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=1024*512, backupCount=1)
-# handler.setFormatter(FORMAT)
-# app_logger.addHandler(handler)
-
 var = ('\n'
-	   'Level   Numeric value\n'
-	   'CRITICAL    50\n'
-	   'ERROR   	40\n'
-	   'WARNING 	30\n'
-	   'INFO    	20\n'
-	   'DEBUG   	10\n'
-	   'NOTSET  	0\n')
+       'Level   Numeric value\n'
+       'CRITICAL    50\n'
+       'ERROR   	40\n'
+       'WARNING 	30\n'
+       'INFO    	20\n'
+       'DEBUG   	10\n'
+       'NOTSET  	0\n')
 
 logging.basicConfig(filename=LOG_FILENAME,
-	format= FORMAT,
-	level=logging.DEBUG,
-	datefmt='%m/%d/%Y %I:%M:%S')
+                    format=FORMAT,
+                    level=logging.DEBUG,
+                    datefmt='%m/%d/%Y %I:%M:%S')
+
+
+config = cfg.getConfig()
+
+print_to_screen = config.get('logging', 'print_to_screen')
+# Printing to stdout
+ch = logging.StreamHandler(sys.stdout)
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(logging.Formatter(FORMAT))
+
+app_logger = logging.getLogger(__name__)
+app_logger.setLevel(logging.DEBUG)
+
+if print_to_screen == "true":
+    app_logger.addHandler(ch)
 
 info = app_logger.info
 debug = app_logger.debug
 warning = app_logger.warning
-error =  app_logger.error
+error = app_logger.error
 critical = app_logger.critical
 log = app_logger.log
 exception = app_logger.exception
-
