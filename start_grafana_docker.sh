b8e8aca33c6b4b4a52204adfe40aaed156183e71
Naren Mudivarthy <nmudivar@cisco.com>
2017-08-22 22:33:54 -0700

    Added data mount points for dockers

--- start_grafana_docker.sh
+++ start_grafana_docker.sh
@@ -1,4 +1,4 @@
 docker run -d -i --net=influxdb \
                  -p 3000:3000 \
-                 -v $PWD/grafana:/var/lib/grafana \
+                 -v $PWD/data/grafana:/var/lib/grafana \
                  --name grafana grafana/grafana
--- start_influx_docker.sh
+++ start_influx_docker.sh
@@ -1,6 +1,6 @@
 docker run -d -p 8083:8083 -p 8086:8086 \
           -v $PWD/influxdb.conf:/etc/influxdb/influxdb.conf:ro \
-          -v $PWD/influxdb:/var/lib/influxdb \
+          -v $PWD/data/influxdb:/var/lib/influxdb \
           --name=influxdb \
           --net=influxdb \
           -e INFLUXDB_ADMIN_ENABLED=true \
